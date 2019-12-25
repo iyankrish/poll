1509e1fd7af420212941713f8fe81e5167e4bd26
iyankrish <iyankrish@gmail.com>
2019-12-25 09:20:56 +0530

    Polling app

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+admin.site.site_header ="Ewin Admin"
+admin.site.site_title ="Ewin Admin Area"
+admin.site.index_title="Ewin"
+
+class ChoiceInline(admin.TabularInline):
+    model=Choice
+    extra=3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets =[(None,{'fields':["question_text"]}),
+    ("Date Innformation",{'fields':['pub_date'],'classes':['collapse']}),]
+    inlines=[ChoiceInline]
+
+
+# Register your models here.
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question,QuestionAdmin)
